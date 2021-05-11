f8f75df937826c108af0be957536cb5e80459c2a
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-05-11 16:59:52 -0500

    Que son y como se utilizan las rutas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Ruta por defecto
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Funcion anonima o Closure
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hola desde la pagina de inicio';
+});
+
+Route::get('contacto', function () {
+    return 'Hola desde la pagina de contacto';
+});
+
+// Tipos de rutas
+// Route::get();
+// Route::post(); // from action='POST'
+// Route::puth();
+// Route::delete();
+
+// Parametro obligatorio
+Route::get('saludo/{nombre}', function ($nombre) {
+    return 'Saludos ' . $nombre;
+});
+
+// Parametro opcional
+Route::get('saludo/{nombre?}', function ($nombre = 'Invitado') {
+    return 'Saludos ' . $nombre;
 });
